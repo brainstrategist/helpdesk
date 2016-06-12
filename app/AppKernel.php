f3a9da67f0caf6e1368396788ffc59bd92c3e5f4
Brunet Sebastien <brunetsebastien33@gmail.com>
2016-06-12 22:56:08 +0200

    Add the liip_imagine bundle support for thumbnail generation

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new WhiteOctober\BreadcrumbsBundle\WhiteOctoberBreadcrumbsBundle(),
+            new Liip\ImagineBundle\LiipImagineBundle(),
             new BrainStrategist\CalendarBundle\BrainStrategistCalendarBundle(),
             new BrainStrategist\KernelBundle\BrainStrategistKernelBundle(),
         ];
