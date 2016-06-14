a6cf09c2c38e95cfce9584ffc1640386a1741044
Brunet Sebastien <brunetsebastien33@gmail.com>
2016-06-14 23:33:57 +0200

    Add the translation process with locale route support

---
+++ src/BrainStrategist/KernelBundle/Resources/translations/messages.en.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'Home' => 'Home',
+    'Some text without link' =>'Some text without link',
+    'Previous'=> 'Previous',
+    'Next'=>'Next',
+    'facebook' => 'facebook',
+    'Hello'=>'Hello'
+);
+
+?>
